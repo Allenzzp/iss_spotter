4e151124946d3664ac058cc76b96b17827ca7600
Allenzzp <131452abc@gmail.com>
2021-12-03 07:05:38 +0000

    implement fetch coordinates based on ip address

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const {fetchMyIP} = require("./iss");
+const {fetchMyIP, fetchCoordsByIP } = require("./iss");
 
-fetchMyIP((err, data) => {
+fetchMyIP((err, ip) => {
   if (err) {
     console.log("It didn't work!", err);
     return;
   }
-  console.log('It worked! Returned IP:', data);
+  console.log('It worked! Returned IP:', ip);
+  fetchCoordsByIP(ip, (err, data) => {
+    if (err) {
+      console.log(`IP: ${ip} didn't work!`, err);
+      return;
+    }
+
+    console.log("The coordinates is", data, "based on IP address:", ip, ".");
+  });
 });
--- iss.js
+++ iss.js
@@ -16,4 +16,26 @@ const fetchMyIP = (callback) => {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://freegeoip.app/json/${ip}`, (err, response, data) => {
+    if (err) return callback(err, null);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching coordinates for IP: ${ip}. Response: ${data}`;
+      return callback(Error(msg), null);
+    }
+
+    const { latitude, longitude } = JSON.parse(data);
+    return callback(null, { latitude, longitude });
+  });
+};
+
+
+
+
+
+
+module.exports = { 
+  fetchMyIP,
+  fetchCoordsByIP
+};
